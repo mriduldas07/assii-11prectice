3c68cbbd67434be0143f0746c82787e8bb772915
mriduldas07 <mriduldas0325@gmail.com>
2022-05-06 10:34:11 +0600

    banner

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import './Banner.css'
 
 const Banner = () => {
     return (
-        <div>
-            <h2>Banner</h2>
-        </div>
+        <Carousel className='container mt-5 banner'>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      style={{height: '600px'}}
+      src="https://i.ibb.co/G5xLj5k/jonas-leupe-w-K-elt11p-F0-unsplash.jpg"
+      alt="First slide"
+    />
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      style={{height: '600px'}}
+      src="https://i.ibb.co/XX3QbQV/priscilla-du-preez-Bjh-Uu6-Bp-UZA-unsplash.jpg"
+      alt="Second slide"
+    />
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      style={{height: '600px'}}
+      src="https://i.ibb.co/7KhYxT7/maxim-ilyahov-0a-Rycsf-H57-A-unsplash.jpg"
+      alt="Third slide"
+    />
+  </Carousel.Item>
+</Carousel>
     );
 };
 
